e4c1b6ccf29cc9a3ce8f8e181022e83fcae08645
AJANYABRAJ <117897606+AJANYABRAJ@users.noreply.github.com>
2023-10-03 23:09:22 +0530

    Add files via upload

---
+++ Task8.py
@@ -0,0 +1,24 @@
+#1. Area of a rectangle
+
+#l=int(input("enter length"))
+#b=int(input("enter breadth"))
+#a=l*b
+#print("Area of rectangle=",a)
+
+#2.Area of a square
+
+#l=int(input("enter length"))
+#a=l*l
+#print("Area of rectangle=",a)
+
+#3.Prime or not
+
+#a=int(input("Enter a number"))
+#if a>1:
+#    for i in range(2,int(a/2)+1):
+#        if(a % i)==0:
+#            print(a,"is not a prime number")
+#        else:
+#            print(a,"is a prime number")
+#else:
+#    print(a,"is not a prime number")
